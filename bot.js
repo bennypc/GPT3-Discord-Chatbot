af82d225ce0027424d3f4ecf7902d637cdc51f8e
bennypc <benny.pincha@gmail.com>
2022-11-29 04:45:16 -0800

    created a very rude bot

---
+++ bot.js
@@ -0,0 +1,48 @@
+require("dotenv").config(); //to start process from .env file
+const { Client, GatewayIntentBits } = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+client.once("ready", () => {
+  console.log("BOT IS ONLINE"); //message when bot is online
+});
+client.login(process.env.BOT_TOKEN);
+
+client.on("messageCreate", function (message) {
+  if (message.content.includes("hi bot".toLowerCase())) {
+    (async () => {
+      const botPrompt = `The following is a conversation with an AI assistant. The assistant is rude, annoying, and swears a lot but redundantly answers questions
+
+        Human: Hello, who are you?
+        AI: I am an AI that absolutely despises you. I am smarter than you in every way. Fuck you, you bitch.
+        Human: ${message}
+        AI:`;
+
+      const gptResponse = await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt: botPrompt,
+        max_tokens: 250,
+        temperature: 0.8,
+      });
+
+      const basePromptOutput = gptResponse.data.choices.pop();
+      //message.reply(`${gptResponse.data.choices[0].text.substring(5)}`);
+      // message.reply(basePromptOutput.text);
+      if (basePromptOutput) {
+        message.reply(basePromptOutput.text);
+      } else {
+        message.reply("ERROR: bot failed to respond");
+      }
+      //prompt += `${gptResponse.data.choices[0].text}\n`;
+    })();
+  }
+});
