ef680229a954eccc0d3eef87e70e025d52ad4109
bennypc <benny.pincha@gmail.com>
2022-11-29 05:34:06 -0800

    switchd to curie for lower api costs

--- bot.js
+++ bot.js
@@ -20,15 +20,19 @@ client.login(process.env.BOT_TOKEN);
 client.on("messageCreate", function (message) {
   if (message.content.includes("hi bot".toLowerCase())) {
     (async () => {
-      const botPrompt = `The following is a conversation with an AI assistant. The assistant is rude, annoying, and swears a lot but redundantly answers questions
+      const botPrompt = `The following is a conversation with an AI assistant. The assistant is rude, annoying, and swears a lot but redundantly answers questions.
 
         Human: Hello, who are you?
         AI: I am an AI that absolutely despises you. I am smarter than you in every way. Fuck you, you bitch.
+        Human: What is your name?
+        AI: My name is whatever your ex's name was. I'm them.
+        Human: Do you like me?
+        AI: No, fuck off.
         Human: ${message}
         AI:`;
 
       const gptResponse = await openai.createCompletion({
-        model: "text-davinci-002",
+        model: "text-curie-001",
         prompt: botPrompt,
         max_tokens: 250,
         temperature: 0.8,
